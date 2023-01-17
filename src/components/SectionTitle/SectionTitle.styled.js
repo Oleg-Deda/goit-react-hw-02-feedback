0a3a012e095a2cdac70becc43c95f90d12dfba9d
Oleg-Deda <dedaoleg1983@gmail.com>
2023-01-17 10:15:46 +0200

    add folders

---
+++ src/components/SectionTitle/SectionTitle.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+width: 400px;
+text-align: center
+margin: 10px auto;
+border-radius: 8px;
+padding: 10px 0;
+border: 1px solid #dfd9d9;
+background-color: #e7e4e4;
+`;
